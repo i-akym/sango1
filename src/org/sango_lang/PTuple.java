d92985dd3ab52eebfcd3c6c20fede40e759e7e29
Isao Akiyama <isao.akiyama@gmail.com>
2022-04-18 20:55:40 +0900

    Refactor

--- src/org/sango_lang/PTuple.java
+++ src/org/sango_lang/PTuple.java
@@ -28,7 +28,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 class PTuple extends PDefaultExprObj {
-  PExpr[] elems;
+  PExprList.Elems elems;
 
   PTuple(Parser.SrcInfo srcInfo) {
     super(srcInfo);
@@ -39,54 +39,53 @@ class PTuple extends PDefaultExprObj {
     buf.append("tuple[src=");
     buf.append(this.srcInfo);
     buf.append(",elems=[");
-    for (int i = 0; i < this.elems.length; i++) {
-      buf.append(this.elems[i]);
-      buf.append(",");
-    }
+    buf.append(this.elems);
     buf.append("]]");
     return buf.toString();
   }
 
-  static class Builder {
-    PTuple tuple;
-    List<PExpr> elemList;
+  static PTuple create(Parser.SrcInfo srcInfo, PExprList.Elems elems) {
+    PTuple t =  new PTuple(srcInfo);
+    t.elems = elems;
+    return t;
+  }
 
-    static Builder newInstance(Parser.SrcInfo srcInfo) {
-      return new Builder(srcInfo);
-    }
+  // static class Builder {
+    // PTuple tuple;
+    // List<PExpr> elemList;
 
-    Builder(Parser.SrcInfo srcInfo) {
-      this.tuple = new PTuple(srcInfo);
-      this.elemList = new ArrayList<PExpr>();
-    }
-
-    // void setSrcInfo(Parser.SrcInfo si) {
-      // this.tuple.srcInfo = si;
+    // static Builder newInstance(Parser.SrcInfo srcInfo) {
+      // return new Builder(srcInfo);
     // }
 
-    void addElem(PExpr elem) {
-      this.elemList.add(elem);
-    }
+    // Builder(Parser.SrcInfo srcInfo) {
+      // this.tuple = new PTuple(srcInfo);
+      // this.elemList = new ArrayList<PExpr>();
+    // }
 
-    void addElemList(List<PExpr> elemList) {
-      for (int i = 0; i < elemList.size(); i++) {
-        this.addElem(elemList.get(i));
-      }
-    }
+    // void setElems(PExprList.Elems elems) {
+      // this.tuple.elems = elems;
+    // }
 
-    PTuple create() throws CompileException {
-      StringBuffer emsg;
-      if (this.elemList.size() < 2) {
-        emsg = new StringBuffer();
-        emsg.append("Insufficient expressions at ");
-        emsg.append(this.tuple.srcInfo);
-        emsg.append(".");
-        throw new CompileException(emsg.toString());
-      }
-      this.tuple.elems = this.elemList.toArray(new PExpr[this.elemList.size()]); 
-      return this.tuple;
-    }
-  }
+    // void addElemList(List<PExpr> elemList) {
+      // for (int i = 0; i < elemList.size(); i++) {
+        // this.addElem(elemList.get(i));
+      // }
+    // }
+
+    // PTuple create() throws CompileException {
+      // StringBuffer emsg;
+      // if (this.elemList.size() < 2) {
+        // emsg = new StringBuffer();
+        // emsg.append("Insufficient expressions at ");
+        // emsg.append(this.tuple.srcInfo);
+        // emsg.append(".");
+        // throw new CompileException(emsg.toString());
+      // }
+      // // this.tuple.elems = this.elemList.toArray(new PExpr[this.elemList.size()]); 
+      // return this.tuple;
+    // }
+  // }
 
   static PTuple accept(ParserA.TokenReader reader, int spc) throws CompileException, IOException {
     StringBuffer emsg;
@@ -94,27 +93,33 @@ class PTuple extends PDefaultExprObj {
     if ((t = ParserA.acceptToken(reader, LToken.LPAR_VBAR, spc)) == null) {
       return null;
     }
-    Builder builder = Builder.newInstance(t.getSrcInfo());
-    builder.addElemList(PExpr.acceptPosdSeq(reader, 2));
-    if (ParserA.acceptToken(reader, LToken.VBAR_RPAR, ParserA.SPACE_DO_NOT_CARE) == null) {
+    PExprList.Elems es = PExprList.acceptElems(reader, reader.getCurrentSrcInfo(), 2);
+    if (es == null) {
       emsg = new StringBuffer();
-      emsg.append("Syntax error at ");
+      emsg.append("Invalid tuple specification at ");
       emsg.append(reader.getCurrentSrcInfo());
       emsg.append(".");
-      ParserA.Token et = reader.getToken();
-      if (et.value != null) {
-        emsg.append(" - ");
-        emsg.append(et.value.token);
-      }
       throw new CompileException(emsg.toString());
     }
-    return builder.create();
+    // Builder builder = Builder.newInstance(t.getSrcInfo());
+    // builder.addElemList(PExpr.acceptPosdSeq(reader, 2));
+    if (ParserA.acceptToken(reader, LToken.VBAR_RPAR, ParserA.SPACE_DO_NOT_CARE) == null) {
+      emsg = new StringBuffer();
+      emsg.append("Missing end of tuple at ");
+      emsg.append(reader.getCurrentSrcInfo());
+      emsg.append(".");
+      throw new CompileException(emsg.toString());
+    }
+    return create(t.getSrcInfo(), es);
+    // return builder.create();
   }
 
   static PTuple acceptX(ParserB.Elem elem) throws CompileException {
     StringBuffer emsg;
     if (!elem.getName().equals("tuple")) { return null; }
-    Builder builder = Builder.newInstance(elem.getSrcInfo());
+    Parser.SrcInfo si = elem.getSrcInfo();
+    List<PExpr> es = new ArrayList<PExpr>();
+    // Builder builder = Builder.newInstance(elem.getSrcInfo());
     ParserB.Elem e = elem.getFirstChild();
     while (e != null) {
       PExpr expr = PExpr.acceptX(e);
@@ -124,54 +129,48 @@ class PTuple extends PDefaultExprObj {
         emsg.append(e.getSrcInfo().toString());
         throw new CompileException(emsg.toString());
         }
-      builder.addElem(expr);
+      es.add(expr);
+      // builder.addElem(expr);
       e = e.getNextSibling();
     }
-    return builder.create();
+    return create(si, PExprList.Elems.create(si, es));
+    // return builder.create();
   }
 
   public void setupScope(PScope scope) {
     if (scope == this.scope) { return; }
     this.scope = scope;
     this.idResolved = false;
-    for (int i = 0; i < this.elems.length; i++) {
-      this.elems[i].setupScope(scope);
-    }
+    this.elems.setupScope(scope);
   }
 
   public void collectModRefs() throws CompileException {
-    for (int i = 0; i < this.elems.length; i++) {
-      this.elems[i].collectModRefs();
-    }
+    this.elems.collectModRefs();
   }
 
   public PTuple resolve() throws CompileException {
     if (this.idResolved) { return this; }
-    for (int i = 0; i < this.elems.length; i++) {
-      this.elems[i] = this.elems[i].resolve();
-    }
+    this.elems = this.elems.resolve();
     this.idResolved = true;
     return this;
   }
 
   public void normalizeTypes() throws CompileException {
-    for (int i = 0; i < this.elems.length; i++) {
-      this.elems[i].normalizeTypes();
-    }
+    this.elems.normalizeTypes();
   }
 
   public PTypeGraph.Node setupTypeGraph(PTypeGraph graph) {
-    this.typeGraphNode = graph.createTupleNode(this, this.elems.length);
-    for (int i = 0; i < this.elems.length; i++) {
-      ((PTypeGraph.TupleNode)this.typeGraphNode).setElemNode(i, this.elems[i].setupTypeGraph(graph));
+    this.typeGraphNode = graph.createTupleNode(this, this.elems.exprs.length);
+    for (int i = 0; i < this.elems.exprs.length; i++) {
+      ((PTypeGraph.TupleNode)this.typeGraphNode).setElemNode(i, this.elems.exprs[i].setupTypeGraph(graph));
     }
     return this.typeGraphNode;
   }
 
   public GFlow.Node setupFlow(GFlow flow) {
     GFlow.TupleNode node = flow.createNodeForTuple(this.srcInfo);
-    for (int i = 0; i < this.elems.length; i++) {
-      node.addChild(this.elems[i].setupFlow(flow));
+    for (int i = 0; i < this.elems.exprs.length; i++) {
+      node.addChild(this.elems.exprs[i].setupFlow(flow));
     }
     return node;
   }
