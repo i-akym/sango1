8592a4013c71dcc2eb4588a1065305c66b4b3986
Isao Akiyama <isao.akiyama@gmail.com>
2024-04-13 06:51:15 +0900

    Release 1.7.3_1

--- src/org/sango_lang/Version.java
+++ src/org/sango_lang/Version.java
@@ -26,7 +26,7 @@ package org.sango_lang;
 public class Version {
   static final int MAJOR = 1;
   static final int MINOR = 7;
-  static final int MICRO = 2;
+  static final int MICRO = 3;
   static final String LEVEL = null;
   static final int BUILD = 1;
 
